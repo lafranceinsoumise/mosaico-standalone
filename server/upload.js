5fc540d57b0b8daca2b2f811ec190563f711aaba
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-10-10 12:45:29 +0200

    fix: broken file upload with jquery-file-upload-middleware

--- server/upload.js
+++ server/upload.js
@@ -3,15 +3,16 @@ import upload from "jquery-file-upload-middleware";
 
 import config from "../config.js";
 
-var app = express.Router();
+const app = express.Router();
 
-var uploadOptions = {
+const uploadOptions = {
   tmpDir: ".tmp",
   uploadDir: "./uploads",
   uploadUrl: "/uploads",
   imageVersions: { thumbnail: { width: 90, height: 90 } },
   ssl: config.ssl,
 };
+
 app.use("/", upload.fileHandler(uploadOptions));
 
 export default app;
